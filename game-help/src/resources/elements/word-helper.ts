1ad4c6aed633035d9ff79fefadfa2ce4c740ff59
Jeff <81781741+jeffcrone@users.noreply.github.com>
2022-01-19 22:08:40 -0600

    Modified to use the utility class.

--- game-help/src/resources/elements/word-helper.ts
+++ game-help/src/resources/elements/word-helper.ts
@@ -1,9 +1,23 @@
 import {bindable, autoinject} from 'aurelia-framework';
 import {WordHelperUtility} from 'resources/utilities/word-helper-utility';
 
+/**
+ * Class for the word-helper attribute.
+ */
 @autoinject
 export class WordHelper {
-	constructor(private wordHelperUtility: WordHelperUtility) {
-		//
+
+	/**
+	 * A list of the valid words considering the restrictions that have been given.
+	 * @type {string[]}
+	 */
+	private _validWords: string[];
+
+	/**
+	 * Constructor for WordHelperUtility
+	 * @param {WordHelperUtility} _wordHelperUtility A utility class for word helper.
+	 */
+	constructor(private _wordHelperUtility: WordHelperUtility) {
+		this._validWords = this._wordHelperUtility.getWords();
 	}
 }
--- game-help/src/resources/utilities/word-helper-utility.ts
+++ game-help/src/resources/utilities/word-helper-utility.ts
@@ -1,5 +1,26 @@
+/**
+ * Mostly just some helper classes
+ */
 export class WordHelperUtility {
+
+	/**
+	 * A list of the valid words considering the restrictions that have been given.
+	 * @type {string[]}
+	 */
+	private _validWords: string[];
+
+	/**
+	 * Constructor for WordHelperUtility
+	 */
 	constructor() {
-		//
+		this._validWords = ["abc", "def"];// Temp assignment for now.
+	}
+
+	/**
+	 * Returns the current list of valid words.
+	 * @returns {string[]} The current list of valid words
+	 */
+	public getWords(): string[] {
+		return this._validWords;
 	}
 }
